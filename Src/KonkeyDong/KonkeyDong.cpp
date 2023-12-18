4fe2a9e42c86fe1bd1f9f51984d5127f41cb157c
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2023-12-18 14:28:38 +0100

    Add project files.

---
+++ Src/KonkeyDong/KonkeyDong.cpp
@@ -0,0 +1,12 @@
+﻿// KonkeyDong.cpp : Defines the entry point for the application.
+//
+
+#include "KonkeyDong.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
