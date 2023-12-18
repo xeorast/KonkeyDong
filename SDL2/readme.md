4bb28ecb6b91e0784367bb0cb27cfff055835aa9
Przemek Urbanek <73723125+xeorast@users.noreply.github.com>
2023-12-18 14:57:38 +0100

    Reference SDL2

---
+++ SDL2/readme.md
@@ -0,0 +1,4 @@
+# Here you should place SDL2 files,
+namely `cmake`, `lib` and `include` folders.
+
+Tested with SDL2 version 2.28.5.
--- Src/KonkeyDong/KonkeyDong.cpp
+++ Src/KonkeyDong/KonkeyDong.cpp
@@ -3,10 +3,41 @@
 
 #include "KonkeyDong.h"
 
+#include <SDL.h>
+#include <SDL_main.h>
+
 using namespace std;
 
-int main()
+int main(int argsc, char* args[])
 {
-	cout << "Hello CMake." << endl;
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		printf("SDL_Init error: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	SDL_Window* pWindow = SDL_CreateWindow("Main window",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		640, 480, 0);
+
+	if (pWindow == NULL) {
+		SDL_Quit();
+		printf("SDL_CreateWindow error: %s\n", SDL_GetError());
+		return 1;
+	};
+
+	bool quit = false;
+	while (!quit)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type) {
+			case SDL_QUIT:
+				quit = 1;
+				break;
+			};
+		};
+	}
+
 	return 0;
 }
